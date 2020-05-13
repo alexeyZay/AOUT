92e3f101a3967996655fa1a832592335525dffd6
alexey.z <myboxfm@gmail.com>
2020-05-14 00:55:05 +0300

    splitting one method to separate manager to work with files

--- logAn/LogAnalyzer.cs
+++ logAn/LogAnalyzer.cs
@@ -6,6 +6,15 @@ namespace logAn
     {
         public bool IsFileNameValid (string fileName)
         {
+            FileExtentionManager mngr = new FileExtentionManager();
+            
+            return mngr.IsValid(fileName);
+        }
+    }
+    public class FileExtentionManager 
+    {
+        internal bool IsValid(string fileName)
+        {
             if (String.IsNullOrEmpty(fileName))
             {
                 throw new ArgumentException("No fileName provided");
